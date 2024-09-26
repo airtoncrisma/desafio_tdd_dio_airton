a77f3fb52fb6a91457119f2d65032fcbd6959c9b
airtoncrisma <atopomap@outlook.com>
2024-09-26 16:35:37 -0300

    correct

--- Calculadora.tests/CalculadoraTests.cs
+++ Calculadora.tests/CalculadoraTests.cs
@@ -5,7 +5,7 @@ namespace Calculadora.tests
 {
     public class CalculadoraTests
     {
-        public Calculadora clase()
+        public Calculadora classe()
         {
             string data = new DateTime().ToString();
             Calculadora calc = new Calculadora(data);
